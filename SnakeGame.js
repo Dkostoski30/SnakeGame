6138b0a6b3828712ac3fba01137f55e465a63a9d
Daniel Kostoski <116521337+Dkostoski30@users.noreply.github.com>
2023-10-25 12:19:53 +0200

    Update SnakeGame.js
    Signed-off-by: Daniel Kostoski <116521337+Dkostoski30@users.noreply.github.com>

--- SnakeGame.js
+++ SnakeGame.js
@@ -196,7 +196,7 @@ class Nivo {
             }else{
                 this.snake.dvizi();
             }
-            await delay(160);
+            await delay(90);
             this.updateScore(x);
             if(this.snake.hasColided()){
                 let gameover = document.getElementById('GameOver');
